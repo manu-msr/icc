38d0a933be76709b8aa6971fb82608cf09953d91
manu-msr <manu@ciencias.unam.mx>
2016-10-02 22:38:54 -0500

    Edita identación.

--- ConvertidorEfe/ConvertidorEfe.java
+++ ConvertidorEfe/ConvertidorEfe.java
@@ -8,49 +8,49 @@ import java.io.IOException;
  */
 public class ConvertidorEfe {
 
-	/* Atributo que almacena el contenido del archivo. */
-	private String contenido;
+   /* Atributo que almacena el contenido del archivo. */
+   private String contenido;
 
-	/**
-	 * Constructor único de un convertidor.
-	 */
-	public ConvertidorEfe() {
-		contenido = "";
-	}
+   /**
+    * Constructor único de un convertidor.
+    */
+   public ConvertidorEfe() {
+      contenido = "";
+   }
 
-	/**
-	 * Método que toma una palabra y la convierte a «Efe».
-	 * @param cadena Palabra a convertir.
-	 * @return Palabra convertida a «Efe».
-	 */
-	public String convierte(String palabra) {
-		palabra = palabra.replaceAll("a", "afa");
-		palabra = palabra.replaceAll("e", "efe");
-		palabra = palabra.replaceAll("i", "ifi");
-		palabra = palabra.replaceAll("o", "ofo");
-		palabra = palabra.replaceAll("u", "ufu");
-		return palabra;
-	}
+   /**
+    * Método que toma una palabra y la convierte a «Efe».
+    * @param cadena Palabra a convertir.
+    * @return Palabra convertida a «Efe».
+    */
+   public String convierte(String palabra) {
+      palabra = palabra.replaceAll("a", "afa");
+      palabra = palabra.replaceAll("e", "efe");
+      palabra = palabra.replaceAll("i", "ifi");
+      palabra = palabra.replaceAll("o", "ofo");
+      palabra = palabra.replaceAll("u", "ufu");
+      return palabra;
+   }
 
-	/**
-	 * Método que guarda el contenido del archivo.
-	 * @param out La salida dónde hay que guardar el contenido.
-     * @throws IOException si un error de entrada/salida ocurre.
-     */
-	public void guarda(BufferedWriter out) throws IOException {
-		out.write(convierte(contenido));
-	}
+   /**
+    * Método que guarda el contenido del archivo.
+    * @param out La salida dónde hay que guardar el contenido.
+    * @throws IOException si un error de entrada/salida ocurre.
+    */
+   public void guarda(BufferedWriter out) throws IOException {
+      out.write(convierte(contenido));
+   }
 
-	/**
-	 * Método que carga el archivo de la entrada recibida.
-	 * @param in La entrada dónde hay que cargar el contenido.
-	 * @throws IOException si un error de entrada/salida ocurre.
-	 */
-	public void carga(BufferedReader in) throws IOException {
-		String linea = in.readLine();
-		while (linea != null) {
-			contenido += linea + "\n";
-			linea = in.readLine();
-		}
-	}
+   /**
+    * Método que carga el archivo de la entrada recibida.
+    * @param in La entrada dónde hay que cargar el contenido.
+    * @throws IOException si un error de entrada/salida ocurre.
+    */
+   public void carga(BufferedReader in) throws IOException {
+      String linea = in.readLine();
+      while (linea != null) {
+         contenido += linea + "\n";
+         linea = in.readLine();
+      }
+   }
 }
--- ConvertidorEfe/PruebaEfe.java
+++ ConvertidorEfe/PruebaEfe.java
@@ -12,34 +12,34 @@ import java.util.Scanner;
  */
 public class PruebaEfe {
 
-	public static void main(String[] args) {
-		/* Creamos el convertidor. */
-		ConvertidorEfe cfe = new ConvertidorEfe();
-		/* Creamos el objeto para pedir datos del teclado. */
-		Scanner teclado = new Scanner(System.in);
-		/* Almacenamos el primer argumento de la línea de comandos. */
-		String archivo = args[0];
-		/* Metemos este código en un bloque try-catch por si hay errores de
-		   entrada salida. */
-		try {
-			/* Carga el archivo. */
-			FileInputStream fileIn = new FileInputStream(archivo);
-			InputStreamReader isIn = new InputStreamReader(fileIn);
-			BufferedReader in = new BufferedReader(isIn);
-			cfe.carga(in);
-			in.close();
-			System.out.printf("Archivo %s cargado\n\n", archivo);
+   public static void main(String[] args) {
+      /* Creamos el convertidor. */
+      ConvertidorEfe cfe = new ConvertidorEfe();
+      /* Creamos el objeto para pedir datos del teclado. */
+      Scanner teclado = new Scanner(System.in);
+      /* Almacenamos el primer argumento de la línea de comandos. */
+      String archivo = args[0];
+      /* Metemos este código en un bloque try-catch por si hay errores de
+         entrada salida. */
+      try {
+         /* Carga el archivo. */
+         FileInputStream fileIn = new FileInputStream(archivo);
+         InputStreamReader isIn = new InputStreamReader(fileIn);
+         BufferedReader in = new BufferedReader(isIn);
+         cfe.carga(in);
+         in.close();
+         System.out.printf("Archivo %s cargado\n\n", archivo);
 
-			/* Guarda el archivo. */
-			System.out.print("Introduce el nombre del archivo de salida: ");
-			String salida = teclado.next();
-			FileOutputStream fileOut = new FileOutputStream(salida);
-			OutputStreamWriter osOut = new OutputStreamWriter(fileOut);
-			BufferedWriter out = new BufferedWriter(osOut);
-			cfe.guarda(out);
-			out.close();
-		} catch (IOException ioe) {
-			System.exit(1);
-		}
-	}
+         /* Guarda el archivo. */
+         System.out.print("Introduce el nombre del archivo de salida: ");
+         String salida = teclado.next();
+         FileOutputStream fileOut = new FileOutputStream(salida);
+         OutputStreamWriter osOut = new OutputStreamWriter(fileOut);
+         BufferedWriter out = new BufferedWriter(osOut);
+         cfe.guarda(out);
+         out.close();
+      } catch (IOException ioe) {
+         System.exit(1);
+      }
+   }
 }
