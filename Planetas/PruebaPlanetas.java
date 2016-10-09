ab31e3b91c6ec3d620ea2efd0d68f50495d912ad
manu-msr <manu@ciencias.unam.mx>
2016-10-09 18:04:42 -0500

    Corrige identación.

--- Planetas/PruebaPlanetas.java
+++ Planetas/PruebaPlanetas.java
@@ -3,50 +3,50 @@
  */
 public class PruebaPlanetas {
 
-	/**
-	 * Método que regresa la representación en cadena de un planeta.
-	 * @param campo Campo a convertir.
-	 */
-	public static void toString(Enum campo) {
-		if (campo instanceof Planeta) { // verificamos que sea instancia
-			Planeta planeta = (Planeta)campo; // hacemos el cast
-			switch (planeta) { // hacemos switch sobre cualquier posible valor
-				case MERCURIO:
-					System.out.println("Mercurio");
-					break;
-				case VENUS:
-					System.out.println("Venus");
-					break;
-				case TIERRA:
-					System.out.println("Tierra");
-					break;
-				case MARTE:
-					System.out.println("Marte");
-					break;
-				case JUPITER:
-					System.out.println("Jupiter");
-					break;
-				case SATURNO:
-					System.out.println("Saturno");
-					break;
-				case URANO:
-					System.out.println("Urano");
-					break;
-				case NEPTUNO:
-					System.out.println("Neptuno");
-					break;
-				case PLUTON:
-					System.out.println("Plutón");
-					break;
-				default: // en caso de recibir un dato inválido.
-					System.out.println("Valor incorrecto.");
-					break;
-			}
-		} else // si el parámetro no es de tipo inválido.
-			throw new IllegalArgumentException();
-	}
+   /**
+    * Método que regresa la representación en cadena de un planeta.
+    * @param campo Campo a convertir.
+    */
+   public static void toString(Enum campo) {
+      if (campo instanceof Planeta) { // verificamos que sea instancia
+         Planeta planeta = (Planeta)campo; // hacemos el cast
+         switch (planeta) { // hacemos switch sobre cualquier posible valor
+            case MERCURIO:
+               System.out.println("Mercurio");
+               break;
+            case VENUS:
+               System.out.println("Venus");
+               break;
+            case TIERRA:
+               System.out.println("Tierra");
+               break;
+            case MARTE:
+               System.out.println("Marte");
+               break;
+            case JUPITER:
+               System.out.println("Jupiter");
+               break;
+            case SATURNO:
+               System.out.println("Saturno");
+               break;
+            case URANO:
+               System.out.println("Urano");
+               break;
+            case NEPTUNO:
+               System.out.println("Neptuno");
+               break;
+            case PLUTON:
+               System.out.println("Plutón");
+               break;
+            default: // en caso de recibir un dato inválido.
+               System.out.println("Valor incorrecto.");
+               break;
+         }
+      } else // si el parámetro no es de tipo inválido.
+         throw new IllegalArgumentException();
+   }
 
-	public static void main(String[] args) {
-		toString(Planeta.TIERRA); // llamada para la prueba.
-	}
+   public static void main(String[] args) {
+      toString(Planeta.TIERRA); // llamada para la prueba.
+   }
 }
