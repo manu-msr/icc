f1ab46d710ba082beb44435eebbf6bc1444a2e86
manu-msr <manu@ciencias.unam.mx>
2016-10-09 18:00:08 -0500

    Agrega ejemplo enumeraciones.

---
+++ Planetas/Planeta.java
@@ -0,0 +1,24 @@
+/**
+ * Enumeración para representar a un planeta.
+ */
+public enum Planeta {
+
+	/** El planeta mercurio. */
+	MERCURIO,
+	/** El planeta venus. */
+	VENUS,
+	/** El planeta tierra. */
+	TIERRA,
+	/** El planeta marte. */
+	MARTE,
+	/** El planeta jupiter. */
+	JUPITER,
+	/** El planeta saturno. */
+	SATURNO,
+	/** El planeta urano. */
+	URANO,
+	/** El planeta neptuno. */
+	NEPTUNO,
+	/** El planeta plutón. */
+	PLUTON;
+}
